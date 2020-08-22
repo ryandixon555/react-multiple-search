9483260d33a7caa0ee6ef75bd62fd1c5472885d6
rdixon555 <ryan555@sky.com>
2020-08-22 12:49:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useState } from "react";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import Information from './info-json'
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+export default function App() {
+  
+  const [filterObj, setFilterObj] = useState({name:'', age:'', keywords:''}),
+        onFilter = ({target:{name, value}}) => 
+          setFilterObj({...filterObj, [name]: value})
+  return (
+    <div>
+      <label>Name</label>
+      <input name="name" value={filterObj.name} onChange={onFilter} />
+      <label>Keywords</label>
+      <input name="keywords" value={filterObj.keywords} onChange={onFilter} />
+      <label>Age</label>
+      <input type="number" name="age" value={filterObj.age} onChange={onFilter} />
+      <ul>
+        {
+          Information.map(({id, name, age, keywords}) => {
+            const match = (name.toLowerCase().includes(filterObj.name.toLowerCase()) || !filterObj.name) &&
+                    (`${age}`.includes(filterObj.age) || !filterObj.age) &&
+                    (keywords.some(w => w.toLowerCase().includes(filterObj.keywords.toLowerCase())) || !filterObj.keywords)
+             return match && <li key={id}>{name}, {age} ({keywords.join(', ')})</li>
+          })
+        }
+      </ul>
+    </div>
+  )
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
---
+++ src/info-json.js
@@ -0,0 +1,34 @@
+const Information = [
+  {
+    "id": 1,
+    "name":"Bob",
+    "age": 21,
+    "keywords": ["USA",]
+  },
+  {
+    "id": 2,
+    "name":"Sam",
+    "age":19,
+    "keywords": ["Ghana"]
+  },
+  {
+    "id": 3,
+    "name":"Mark",
+    "age":17,
+    "keywords": ["Africa","England"]
+  },
+  {
+    "id": 4,
+    "name":"Markus",
+    "age":21,
+    "keywords": ["Africa","England"]
+  },
+  {
+    "id": 5,
+    "name":"Markus",
+    "age":29,
+    "keywords": ["Africa","Nigeria"]
+  }
+];
+
+export default Information;
